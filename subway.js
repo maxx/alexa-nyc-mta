f2bae5d3b1fe4163ecb846dfa2f8b6bd7c23a475
James Vautin <james@vautin.com>
2015-09-16 01:59:10 +0000

    re-read status file on each request .. optimize later

--- subway.js
+++ subway.js
@@ -2,43 +2,43 @@ var express = require('express');
 var app = express();
 fs = require('fs')
 
-fs.readFile('serviceStatus.txt', 'utf8', function (err,data,res) {
-  var parseString = require('xml2js').parseString;
 
-  parseString(data, function (err, result) {
-  //console.log(result);
-  if (err) {
-    return console.log(err);
-  }
-  console.log("result: ",result["service"]["subway"]);
+app.post('/subway', function (req, res) {
+  fs.readFile('serviceStatus.txt', 'utf8', function (err,data) {
+    var parseString = require('xml2js').parseString;
 
-  app.post('/subway', function (req, res) {
-  console.log("Got request: %o", req);
+    parseString(data, function (err, result) {
+    //console.log(result);
+    if (err) {
+      return console.log(err);
+    }
+    console.log("result: ",result["service"]["subway"]);
 
-  say_this = "";
+    console.log("Got request: %o", req);
 
-  for (var i=0; i<result["service"]["subway"][0]["line"].length;i++) {
-    line = result["service"]["subway"][0]["line"][i];
-    say_this = say_this.concat(line["name"][0].replace(/(\S{1})/g,"$1 "))
-    say_this = say_this.concat("line has ")
-    say_this = say_this.concat(line["status"][0])
-    say_this = say_this.concat(", ")
-  };
+    say_this = "";
 
-  console.log("say this is:", say_this)
+    for (var i=0; i<result["service"]["subway"][0]["line"].length;i++) {
+      line = result["service"]["subway"][0]["line"][i];
+      say_this = say_this.concat(line["name"][0].replace(/(\S{1})/g,"$1 "))
+      say_this = say_this.concat("line has ")
+      say_this = say_this.concat(line["status"][0])
+      say_this = say_this.concat(", ")
+    };
 
-  text = {"outputSpeech": {"type": "PlainText", "text": say_this}}
-  response = { 
-    "version": "0.1", 
-//  "sessionAttributes": { "someInfoOnPreviousRequest": { "last_line": "something"   } },
-    "response": text,
-    "shouldEndSession": true  
-  } 
+    console.log("say this is:", say_this)
 
-  console.log("Sending response: ", JSON.stringify(response))
-    res.send(JSON.stringify(response));
-  });
+    text = {"outputSpeech": {"type": "PlainText", "text": say_this}}
+    response = { 
+      "version": "0.1", 
+//    "sessionAttributes": { "someInfoOnPreviousRequest": { "last_line": "something"   } },
+      "response": text,
+      "shouldEndSession": true  
+    } 
 
+    console.log("Sending response: ", JSON.stringify(response))
+      res.send(JSON.stringify(response));
+    });
   });
 });
 
@@ -46,6 +46,6 @@ var server = app.listen(4445, function () {
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+  console.log('alexa-nyc-mta skill listening at http://%s:%s', host, port);
 });
 
