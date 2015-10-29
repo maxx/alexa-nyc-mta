f6a793c55a18978cc12d82401a9c9992bb952d84
James Vautin <james@vautin.com>
2015-10-29 03:10:41 +0000

    lines grouped by service issue now

--- subway.rb
+++ subway.rb
@@ -7,6 +7,23 @@ require 'json'
 
 require_relative 'lib/alexa_nyc_mta'
 
+def lines_from_state(lines) 
+   line_text = String.new
+   # for line in delayed_lines do
+   lines.each_with_index do |line, idx|
+     if (idx != (lines.count-1))
+        line_text << line.gsub(/(.{1})/, '\1 ')
+        if (idx != lines.count-2) then line_text << ", " end
+        puts "idx is #{idx} lines count is #{lines.count}"
+        puts line
+     else
+        line_text << " and the " + line.gsub(/(.{1})/, '\1 ') 
+        puts line
+      end
+   end
+   line_text
+end
+
 
 post '/subway' do 
    begin
@@ -32,33 +49,24 @@ post '/subway' do
                    delayed_line = delayed_lines[0].gsub(/(.{1})/, '\1 ')
                    say_this << "The #{delayed_line} is currently experiencing delays. " 
                 else
-                   delayed_lines.each do |delayed_line| 
-                      delayed_line = delayed_line.gsub(/(.{1})/, '\1 ')
-                      say_this << "The #{delayed_line} is currently experiencing delays.  " 
-                   end
+                   say_this << "The #{lines_from_state(delayed_lines)} are currently experiencing delays.  " 
                 end
              end
              if (planned_work_lines && planned_work_lines.count > 0)
                 puts "planned_work_lines are #{planned_work_lines}"
                 if (planned_work_lines.count == 1)
-                   planned_work_litne = planned_work_lines[0].gsub(/(.{1})/, '\1 ')
+                   planned_work_line = planned_work_lines[0].gsub(/(.{1})/, '\1 ')
                    say_this << "The #{planned_work_line} is currently undergoing planned work.  " 
                 else
-                   planned_work_lines.each do |planned_work_line| 
-                      planned_work_line = planned_work_line.gsub(/(.{1})/, '\1 ')
-                      say_this << "The #{planned_work_line} is currently undergoing planned work.  " 
-                   end
+                   say_this << "The #{lines_from_state(planned_work_lines)} are currently undergoing planned work.  " 
                 end
              end
              if (service_change_lines && service_change_lines.count > 0)
                 if (service_change_lines.count == 1)
-                   service_change_litne = service_change_lines[0].gsub(/(.{1})/, '\1 ')
+                   service_change_line = service_change_lines[0].gsub(/(.{1})/, '\1 ')
                    say_this << "The #{service_change_line} has a major service change.  " 
                 else
-                   service_change_lines.each do |service_change_line| 
-                      service_change_line = service_change_line.gsub(/(.{1})/, '\1 ')
-                      say_this << "The #{service_change_line} has a major service change.  " 
-                   end
+                   say_this << "The #{lines_from_state(service_change_lines)} have major service changes.  " 
                 end
              end
 
